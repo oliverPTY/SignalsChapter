0a0251f93eea8eaf1e3f996fe6a39d9800ee205f
Oliver <oliver50715@gmail.com>
2023-09-28 22:50:02 -0500

    se agrega forma tradional

--- src/app/users/pages/users-page/users-page.component.ts
+++ src/app/users/pages/users-page/users-page.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { Characters } from '../../interfaces/users';
 
 @Component({
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.css']
 })
-export class UsersPageComponent {
+export class UsersPageComponent implements OnInit{
+  public userService = inject(UsersService);
+  public character: any;
 
+  ngOnInit(): void {
+    this.userService.loadPage().subscribe(response =>{
+      console.log('response', response);
+      this.character = response;
+    });
+  }
 }
